9b0155d52714c6e9cff4c14ccc755a1689123738
Ruan Matos <ruan.matos@nsh.com.br>
2023-11-29 09:08:55 -0300

    aaa

--- customProxyMiddleware/utils/StoreCreditToken.js
+++ customProxyMiddleware/utils/StoreCreditToken.js
@@ -60,7 +60,7 @@ requestAdmin.interceptors.request.use(
 
       return config;
     } catch (error) {
-      throw new Error("Erro no login 1234567891011: \n" + error);
+      throw new Error("Erro no login 123456789101112: \n" + error);
     }
   },
   (error) => {
