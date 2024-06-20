b507a6ddd95fdb338c815cbf4ba52cae1097339a
Ruan Matos <ruan.matos@nsh.com.br>
2024-06-20 11:05:33 -0300

    Agr vai

--- SearchPostalCode/index.js
+++ SearchPostalCode/index.js
@@ -3,7 +3,7 @@ const app = require('./src/app')
 const env = require('./src/utils/EnvironmentVariables')
 const dotenv = require('dotenv');
 dotenv.config();
-/* Teste 12345678910111213 */
+/* Teste Ruan */
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 const port = env.PORT
 app.listen(port, function () {  
