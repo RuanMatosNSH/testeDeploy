69023caed25b6a7af8740486b6aaeffb50af9f83
Ruan Matos <ruan.matos@nsh.com.br>
2024-06-20 12:55:19 -0300

    poi

--- customProxyMiddleware/index.js
+++ customProxyMiddleware/index.js
@@ -5,7 +5,7 @@ var app = require('./app/index');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-// Read port from command line, config, or default Agr vai realmente vai
+// Read port from command line, config, or default Agr vai realmente vai aaaa
 var port = (process.argv[2] || (process.env.npm_package_config_port || 3000));
 //awuidjuwh
 console.log('starting server...');
