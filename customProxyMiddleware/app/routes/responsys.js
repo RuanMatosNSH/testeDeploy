d0cf34fae92b5f5b39efcc4723f7fa131c7fe624
Ruan Matos <ruan.matos@nsh.com.br>
2024-06-20 09:12:50 -0300

    Subindo arquivos de deploy

--- customProxyMiddleware/app/routes/responsys.js
+++ customProxyMiddleware/app/routes/responsys.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const express = require("express");
 const router = express.Router();
 const axiConfig = require("../../utils/ResponsysToken");
@@ -5,16 +7,20 @@ const ENV_KEYS = require("../../config/keys").responsysKeys();
 
 router.post("/productOpened", async function (request, response) {
   try {
-    const { siteId, profileEmail, productId } = request.body;
+    const { siteId, profileEmail, productId, brand } = request.body;
     const rsysUsers = ENV_KEYS.users[siteId];
 
     if (rsysUsers) {
       await axiConfig.post(
-        `${ENV_KEYS.host}/folders/${rsysUsers.folderName}/suppData/SUP_Navegacao/members`,
+        `${rsysUsers.host}/folders/${rsysUsers.folderName}/suppData/SUP_Navegacao/members`,
         {
           recordData: {
-            fieldNames: ["EMAIL_NAVEGACAO", "PRODUTO_NAVEGACAO"],
-            records: [[profileEmail, productId]],
+            fieldNames: [
+              "EMAIL_NAVEGACAO",
+              "PRODUTO_NAVEGACAO",
+              "ORIGEM_NAVEGACAO",
+            ],
+            records: [[profileEmail, productId, brand]],
             mapTemplateName: null,
           },
           insertOnNoMatch: true,
@@ -43,15 +49,20 @@ router.post("/productOpened", async function (request, response) {
 
 router.post("/collectionOpened", async function (request, response) {
   try {
-    const { siteId, profileEmail, collectionLink, profileOrigin } = request.body;
+    const { siteId, profileEmail, collectionLink, profileOrigin } =
+      request.body;
     const rsysUsers = ENV_KEYS.users[siteId];
 
     if (rsysUsers) {
       await axiConfig.post(
-        `${ENV_KEYS.host}/folders/${rsysUsers.folderName}/suppData/SUP_Click_Results/members`,
+        `${rsysUsers.host}/folders/${rsysUsers.folderName}/suppData/SUP_Click_Results/members`,
         {
           recordData: {
-            fieldNames: ["EMAIL_CLICK_RESULT", "LINK_CLICK_RESULT", "ORIGEM_CLICK_RESULT"],
+            fieldNames: [
+              "EMAIL_CLICK_RESULT",
+              "LINK_CLICK_RESULT",
+              "ORIGEM_CLICK_RESULT",
+            ],
             records: [[profileEmail, collectionLink, profileOrigin]],
             mapTemplateName: null,
           },
@@ -62,7 +73,53 @@ router.post("/collectionOpened", async function (request, response) {
           rejectUnauthorized: false,
           siteId,
         }
-      )
+      );
+
+      response.status(200).json({ success: true });
+    } else {
+      response
+        .status(400)
+        .json({ message: `The site id ${siteId} does not exist on Responsys` });
+    }
+  } catch (error) {
+    response.status(400).json({
+      message: "Could not get response from external Responsys API",
+      detail: error.detail ? error.detail : "",
+      error,
+    });
+  }
+});
+
+router.post("/sendWishlist", async function (request, response) {
+  try {
+    const { email, skuId, color, origin, archetype, siteId, noDesejo } =
+      request.body;
+    const rsysUsers = ENV_KEYS.users[siteId];
+
+    if (rsysUsers) {
+      await axiConfig.post(
+        `${rsysUsers.host}/folders/${rsysUsers.folderName}/suppData/SUP_Wishlist/members`,
+        {
+          recordData: {
+            fieldNames: [
+              "EMAIL_DESEJO",
+              "PRODUCT_ID_DESEJO",
+              "COR_DESEJO",
+              "ORIGEM_DESEJO",
+              "ARQUETIPO_DESEJO",
+              "NO_DESEJO",
+            ],
+            records: [[email, skuId, color, origin, archetype, noDesejo]],
+            mapTemplateName: null,
+          },
+          insertOnNoMatch: true,
+          updateOnMatch: "REPLACE_ALL",
+        },
+        {
+          rejectUnauthorized: false,
+          siteId,
+        }
+      );
 
       response.status(200).json({ success: true });
     } else {
--- customProxyMiddleware/utils/OCCToken.js
+++ customProxyMiddleware/utils/OCCToken.js
@@ -1,30 +1,35 @@
-const axios = require('axios');
-const querystring = require('querystring');
-const nconf = require('nconf');
+const axios = require("axios");
+const querystring = require("querystring");
+const nconf = require("nconf");
 
-const HOST = nconf.get('atg.server.admin.url') ? String(nconf.get('atg.server.admin.url')).replace('/zaxy', '') : 'https://a7727193c1tst-admin.occa.ocs.oraclecloud.com'
+const HOST = nconf.get("atg.server.admin.url")
+  ? String(nconf.get("atg.server.admin.url")).replace("/zaxy", "")
+  : "https://a7727193c1tst-admin.occa.ocs.oraclecloud.com";
 // Initial config set
-const key = process.env.ENVIROMENT_KEY || 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYzM4OTFlOC1hYzJmLTQ4ZWQtYmYxOS1jNWI0ZDRkNDg5MDAiLCJpc3MiOiJhcHBsaWNhdGlvbkF1dGgiLCJleHAiOjE2NjQ0NTE0NzIsImlhdCI6MTYzMjkxNTQ3Mn0=.vC1eRWYDRtY8a2cVd/4Dn/OSfLEq1Gez86/a0aHmAQM=';
+const key =
+  process.env.ENVIROMENT_KEY ||
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYzM4OTFlOC1hYzJmLTQ4ZWQtYmYxOS1jNWI0ZDRkNDg5MDAiLCJpc3MiOiJhcHBsaWNhdGlvbkF1dGgiLCJleHAiOjE2NjQ0NTE0NzIsImlhdCI6MTYzMjkxNTQ3Mn0=.vC1eRWYDRtY8a2cVd/4Dn/OSfLEq1Gez86/a0aHmAQM=";
 
 const baseAxiosConfig = {
   timeout: 20000,
-  baseURL: HOST
+  baseURL: HOST,
 };
 
 // Axios Global config
-axios.defaults.headers.common['Authorization'] = `Bearer ${key}`;
+axios.defaults.headers.common["Authorization"] = `Bearer ${key}`;
 
 // instance for token manage making requests as Admin
-const requestAdmin = module.exports = axios.create(baseAxiosConfig);
+const requestAdmin = (module.exports = axios.create(baseAxiosConfig));
 
 // Login
 const login = async () => {
   try {
     return await axios({
-      method: 'POST',
+      method: "POST",
       url: `${HOST}/ccadmin/v1/login`,
       data: querystring.stringify({
-        'grant_type': 'client_credentials'})
+        grant_type: "client_credentials",
+      }),
     });
   } catch (e) {
     throw new Error(e);
@@ -32,39 +37,46 @@ const login = async () => {
 };
 
 // Interceptor for setting header in requests
-requestAdmin.interceptors.request.use(async (config) => {
-  try {
-    if (requestAdmin.defaults.metadata) {
-      let date = new Date();
-      let timeout = requestAdmin.defaults.metadata.expiresIn * 1000;
-      if (date - requestAdmin.defaults.metadata.lastRefresh <= timeout) {
-        config.headers.Authorization = requestAdmin.defaults.metadata.token;
-        return config;
+requestAdmin.interceptors.request.use(
+  async (config) => {
+    try {
+      if (requestAdmin.defaults.metadata) {
+        let date = new Date();
+        let timeout = requestAdmin.defaults.metadata.expiresIn * 1000;
+        if (date - requestAdmin.defaults.metadata.lastRefresh <= timeout) {
+          config.headers.Authorization = requestAdmin.defaults.metadata.token;
+          return config;
+        }
       }
-    }
-    const response = await login();
-    let token = `Bearer ${response.data.access_token}`;
-    config.headers.Authorization = token;
-    requestAdmin.defaults.metadata = { token: token, lastRefresh: new Date(), expiresIn: response.data.expires_in };
+      const response = await login();
+      let token = `Bearer ${response.data.access_token}`;
+      config.headers.Authorization = token;
+      requestAdmin.defaults.metadata = {
+        token: token,
+        lastRefresh: new Date(),
+        expiresIn: response.data.expires_in,
+      };
 
-    return config;
-  } catch (error) {
-    throw new Error('Erro no login: \n' + error);
+      return config;
+    } catch (error) {
+      throw new Error("Erro no login: \n" + error);
+    }
+  },
+  (error) => {
+    throw new Error("Erro no pré-login: \n" + error);
   }
-}, (error) => {
-  throw new Error('Erro no pré-login: \n' + error);
-}
 );
 
 // Interceptor for response treatment
-requestAdmin.interceptors.response.use(async (response) => {
-  return response.data;
-}, (error) => {
-  if (error.response.status === 401) {
-
+requestAdmin.interceptors.response.use(
+  async (response) => {
+    return response.data;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+    }
+    throw new Error(JSON.stringify(error.config, undefined, 2));
   }
-  throw new Error(JSON.stringify(error.config, undefined, 2));
-}
 );
 
 // module.exports = {
